b98a2e0113fc0282aa2fce2d5a16cc880e976acb
Jose Rafael <51763157+joserafa78@users.noreply.github.com>
2024-08-26 20:00:39 +0000

    Carga nueva linea URL en Configuracion

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 
-define("URL", "http://localhost/EntornosVirtualesyFunciones/");
+//define("URL", "http://localhost/EntornosVirtualesyFunciones/");
+define("URL", "https://legendary-waddle-xxvxpvrqp63vvvr-3000.app.github.dev/");
 
 // ________________ SESIÓN_____________________
 session_start(); // Inicia un Sesion.
